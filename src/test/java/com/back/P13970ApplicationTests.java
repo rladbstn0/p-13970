6bb725edabf20368162202b4096933b755c95fe6
user.email <younsoo6103@naver.com>
2025-06-25 12:34:30 +0900

    work

---
+++ src/test/java/com/back/P13970ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P13970ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
